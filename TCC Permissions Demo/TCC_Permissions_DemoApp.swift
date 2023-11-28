03f075a1f2a22ee8150224641cb43ea81b5d4e4e
Carlo Zottmann <carlo@zottmann.org>
2023-11-28 12:54:35 +0100

    Initial Commit

---
+++ TCC Permissions Demo/TCC_Permissions_DemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  TCC_Permissions_DemoApp.swift
+//  TCC Permissions Demo
+//
+//  Created by Carlo Zottmann on 2023-11-28.
+//
+
+import SwiftUI
+
+@main
+struct TCC_Permissions_DemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
